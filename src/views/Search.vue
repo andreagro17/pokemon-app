28d21bf6f8fe63b81e449fb1b64742058cea36a1
Andrea Cas <andreagro17@gmail.com>
2019-08-07 14:49:06 +0200

    al realizar la busqueda aparecen los nombres de los pokemon

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,24 +1,45 @@
 <template>
     <div>
-        <h1 class='buscar'> Search  </h1>
-        <button v-on:click="fetchPoke">Buscar</button>
+      <div>
+        <div class="prueba">
+          <input
+          type="text"
+          class="prueba__form-control"
+          v-model="textSearch"
+          placeholder='Search...'>
+         <button v-on:click="fetchPoke">Buscar</button>
+         <button v-on:click="pokemonFilter">Buscar_filtro</button>
+          <div v-for="pok2 of pokemonFilter" :key="pok2.name">
+          <span>{{ pok2.name }}</span>
+          </div>
 
-        <div v-for="pok of pokemon" v-bind:key="pok.name">
-            {{ pok.name }} + 'hola'
         </div>
+      <!-- <pokemon-detail :pokemon=`${name}`> -->
+        <pokemon-detail/>
+      </div>
     </div>
 
 </template>
 <script>
 import axios from 'axios'
-
+import PokemonDetail from '@/components/Hero/PokemonDetail'
+// con axios puedo hacer una "request"
+// y pedir aquello del data ¿axios.put?
 export default {
   name: 'Search',
+
+  components: {
+    PokemonDetail
+  },
+
   data () {
     return {
-      pokemon: {}
+      pokemons: [],
+      textSearch: '',
+      searchPokemon: {}
     }
   },
+
   methods: {
     fetchPoke () {
     // https://router.vuejs.org/guide/advanced/data-fetching.html#fetching-after-navigation
@@ -27,11 +48,20 @@ export default {
         .then(resul => {
           console.log('hay alguien?')
           console.log(resul)
-          this.pokemon = resul.data.results
+          this.pokemons = resul.data.results
           console.log('hola')
         })
         .catch(error =>
           console.log(error))
+    } },
+  computed: {
+
+    pokemonFilter () {
+      var textSearch = this.textSearch
+      return this.pokemons.filter(function (el) {
+        return el.name.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1
+      })
     } }
 }
+
 </script>
